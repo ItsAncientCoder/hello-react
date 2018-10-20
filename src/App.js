f101195c811106334a8557aa5a951ad92aae52d0
bandham <bmkantab@gmail.com>
2018-10-20 23:26:52 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  };
+
+  setStateHandle = () => {
+    var item = "Set state";
+    var myArray = [];
+    myArray.push(item);
+    this.setState({ data: myArray });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this.setStateHandle}>SET STATE</button>
+        <h2>{this.state.data}</h2>
       </div>
     );
   }
